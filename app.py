924db82de8b9806cda6181d7cd0ee5e2f4eb398d
eflipe <felipecabaleiro@gmail.com>
2021-01-06 18:55:53 -0300

    Templates

---
+++ app.py
@@ -0,0 +1,4 @@
+from app import app # llamamos al app que es una instancia de flask
+
+if __name__ == '__main__':
+    app.run(debug=False)
---
+++ app/static/js/js_scripts.js
@@ -0,0 +1,3 @@
+$(document).ready(function(){
+  console.log("1 2 3, porbando.");
+});
