29af11131d47bf592d26f8cdb05d926d0ab7d01b
eflipe <felipecabaleiro@gmail.com>
2021-01-14 20:55:11 -0300

    Manejo de errores 404 y 500, envio de emails, threading

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,23 @@
 from flask import render_template, flash, redirect, url_for
+from flask import copy_current_request_context
 from app import app
 from app.forms import SearchForm
 from app import db
+from app import mail
+from flask_mail import Message
 from app.models import AppData
+import threading
+
+
+def send_mail(street_search, timestamp):
+    msg = Message(f'Se busco la calle: {street_search}',
+                  sender='heyheymycode@gmail.com',
+                  recipients=['heyheymycode@gmail.com'])
+    msg.html = render_template('email.html',
+                            street_search=street_search,
+                            timestamp=timestamp)
+
+    mail.send(msg)
 
 
 @app.route('/')
@@ -16,15 +31,22 @@ def index():
 def search():
     form = SearchForm()
     if form.validate_on_submit():
-        flash('Buscaste la calle {}'.format(form.search_item.data))
-        print("DATA FORM", form.search_item.data)
+        print("DATA FORM:", form.search_item.data)
         search_street = form.search_item.data
         search_street = search_street.title()
         street_data = AppData(street_search=search_street)
         db.session.add(street_data)
         db.session.commit()
-#        busqueda_calle = request.form['search_item'] # name="search_item"
 
+        @copy_current_request_context
+        def send_msg(street_search, timestamp):
+            send_mail(street_search, timestamp)
+
+        sender = threading.Thread(name='mail_sender',
+                                  target=send_msg,
+                                  args = (street_data.street_search, street_data.timestamp))
+
+        sender.start()
 
         return render_template("info_street.html",
                                 geo_street=search_street,
--- config.py
+++ config.py
@@ -8,3 +8,10 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_SSL = False
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['heyheymycode@gmail.com']
